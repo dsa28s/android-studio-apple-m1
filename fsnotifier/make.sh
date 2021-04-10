72ecc45ea5e63f08257130093ffbb66c59918840
Dora Lee <dsa28s@naver.com>
2021-04-11 00:28:47 +0900

    feat(intellij): Fork fsnotifier (203.7717.56) and support darwin-arm64

---
+++ fsnotifier/make.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+C_FLAGS="-std=c11 -O2 -Wall -Wextra -Wpedantic -Wno-newline-eof"
+
+clang -arch arm64 -mmacosx-version-min=10.8 -framework CoreServices -std=c11 $C_FLAGS -o fsnotifier fsnotifier.c
+
